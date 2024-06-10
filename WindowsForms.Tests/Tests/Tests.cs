91bebf97d89b38995bed68b79fff33e6619f4c23
AlinaTrofimovicha <linakees2003@gmail.com>
2024-06-10 19:50:59 +0300

    Added form implementation and tests

---
+++ WindowsForms.Tests/Tests/Tests.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WindowsForms.Tests.Tests
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.ebay.com");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Dispose();
+        }
+
+        [Test]
+        public void Test1_field()
+        {
+            IWebElement searchInput = driver.FindElement(By.Id("gh-ac"));
+            Assert.IsNotNull(searchInput, "Search input element not found");
+        }
+
+        [Test]
+        public void Test2_search()
+        {
+            IWebElement searchButton = driver.FindElement(By.Id("gh-btn"));
+            Assert.IsNotNull(searchButton, "Search button element not found");
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,79 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private const string ROOT_URL = "https://www.ebay.com";
+
+        private IWebDriver driver;
+
+
         public Form1()
         {
             InitializeComponent();
+            initDriver();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (driver == null || IsDriverClosed())
+            {
+                initDriver();
+            }
+
+            var searchKeyword = this.textBox1.Text;
+
+            driver.FindElement(By.Id("gh-ac")).Clear();
+            driver.FindElement(By.Id("gh-ac")).SendKeys(searchKeyword);
+            driver.FindElement(By.Id("gh-btn")).Click();
+
+            var url = driver.Url;
+            this.textBox2.Text = url;
+            this.richTextBox1.Text += url + "\n";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (driver == null || IsDriverClosed())
+            {
+                initDriver();
+            }
+
+            driver.Navigate().GoToUrl(ROOT_URL);
+
+            this.textBox1.Clear();
+            this.textBox2.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            driver.Close();
+        }
+
+        private void initDriver()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(ROOT_URL);
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+            base.OnFormClosed(e);
+        }
+
+        private bool IsDriverClosed()
+        {
+            try
+            {
+                var url = driver.Url;
+                return false;
+            }
+            catch (WebDriverException)
+            {
+                return true;
+            }
         }
     }
 }
